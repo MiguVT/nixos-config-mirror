76443d7c0b280f5076819b2fb6c78d3b5e654c39
MiguVT <contacto@miguvt.com>
2026-09-03 18:34:53 +0200

    fix(cpu): use services.scx module for scx_rusty

--- modules/system/scx.nix
+++ modules/system/scx.nix
@@ -1,16 +1,10 @@
 { pkgs, ... }:
 
 {
-  # Rust-based sched-ext schedulers (scx_rusty, scx_p2pq, scxtop, ...)
-  environment.systemPackages = [ pkgs.scx.rustscheds ];
-
   # scx_rusty as the system-wide scheduler (sched_ext)
-  systemd.services.scx_rusty = {
-    description = "SCX Rusty Scheduler";
-    after = [ "local-fs.target" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.scx.rustscheds}/bin/scx_rusty";
-    };
+  services.scx = {
+    enable = true;
+    package = pkgs.scx.rustscheds;
+    scheduler = "scx_rusty";
   };
 }
