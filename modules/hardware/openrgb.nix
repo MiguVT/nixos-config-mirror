6e60de4285121311a4b3581537efc3c639eb5537
MiguVT <contacto@miguvt.com>
2026-09-03 22:03:13 +0200

    fix(openrgb): load i2c-dev and nct6775, add i2c group for user

--- modules/hardware/openrgb.nix
+++ modules/hardware/openrgb.nix
@@ -1,6 +1,17 @@
 { pkgs, ... }:
 
 {
+  boot.kernelModules = [
+    "i2c-dev"
+    # Super I/O: motherboard RGB lives behind SMBus (NCT679x)
+    "nct6775"
+  ];
+
+  users.groups.i2c = {
+    gid = 1010;
+    members = [ "miguvt" ];
+  };
+
   services.hardware.openrgb = {
     enable = true;
     package = pkgs.openrgb-with-all-plugins;
