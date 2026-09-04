cca4597384ec48fcd2be43b980274c2fd4c88000
MiguVT <contacto@miguvt.com>
2026-09-04 02:14:06 +0200

    fix(cpu): disable scx until scx_rusty 1.1.3 is available

--- modules/system/scx.nix
+++ modules/system/scx.nix
@@ -3,7 +3,7 @@
 {
   # scx_rusty as the system-wide scheduler (sched_ext)
   services.scx = {
-    enable = true;
+    enable = false; # TODO: Wait for update, currently 1.1.2 -> 1.1.3 needed for scx_rusty work
     package = pkgs.scx.rustscheds;
     scheduler = "scx_rusty";
   };
