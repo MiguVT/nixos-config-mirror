8056325f0bfa2131e61b6ea208ac3b9109150bde
MiguVT <contacto@miguvt.com>
2026-09-05 01:35:44 +0200

    fix(openrgb): simplify to default enable

--- modules/hardware/openrgb.nix
+++ modules/hardware/openrgb.nix
@@ -1,19 +1,5 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
-  hardware.i2c.enable = true;
-
-  boot.kernelModules = [
-    "i2c-dev"
-    # Super I/O: motherboard RGB lives behind SMBus (NCT679x)
-    "nct6775"
-    "i2c-piix4"
-  ];
-
-  users.users.miguvt.extraGroups = [ "i2c" ];
-
-  services.hardware.openrgb = {
-    enable = true;
-    package = pkgs.openrgb;
-  };
+  services.hardware.openrgb.enable = true;
 }
