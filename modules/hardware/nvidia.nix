77ff77adef2840623bb5c1e3431040317d5b7fa3
MiguVT <contacto@miguvt.com>
2026-09-09 03:53:24 +0200

    fix(nvidia): Set shader disk cache limit to 100GB

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -1,6 +1,12 @@
 { config, ... }:
 
 {
+  # Set system-wide environment variables
+  environment.sessionVariables = {
+    # Expands global NVIDIA shader cache size to 100GB (100 * 1024 * 1024 * 1024 bytes)
+    __GL_SHADER_DISK_CACHE_SIZE = "107374182400";
+  };
+
   hardware.graphics = {
     enable = true;
     # Required for 32-bit Steam games
