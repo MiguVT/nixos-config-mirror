101dae7ad71e7582bafedbf320cef82e9f362a8e
MiguVT <contacto@miguvt.com>
2026-09-16 15:46:44 +0200

    fix(hardware): enable openrazer daemon for RazerGenie

--- modules/hardware/razer.nix
+++ modules/hardware/razer.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 
 {
+  hardware.openrazer.enable = true;
+  hardware.openrazer.users = [ "miguvt" ];
+
   home-manager.users.miguvt.home.packages = with pkgs; [
     razergenie
   ];
