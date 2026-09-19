c05fdda7a4bb4a2872eea41fc9ca50f423267355
MiguVT <contacto@miguvt.com>
2026-09-19 04:18:19 +0200

    chore(nix): Add cuda substituters

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -6,8 +6,19 @@
       "nix-command"
       "flakes"
     ];
-    substituters = [ "https://vicinae.cachix.org" ];
-    trusted-public-keys = [ "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc=" ];
+
+    extra-substituters = [
+      "https://vicinae.cachix.org"
+      "https://cuda-maintainers.cachix.org"
+      "https://nixpkgs-xr.cachix.org"
+    ];
+
+    extra-trusted-public-keys = [
+      "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
+      "cuda-maintainers.cachix.org-1:0dq3bujK38/8qA3S4V0E9vS1y5R="
+      "nixpkgs-xr.cachix.org-1:e1/s9jyDdy0gJgR32zZ2c2P8/GkXvJ="
+    ];
+
     auto-optimise-store = true;
   };
 
