d6b383df12498f25a719b4ad5844658b58829593
MiguVT <contacto@miguvt.com>
2026-09-19 04:30:31 +0200

    fix(building) Set more cores and enable zramswap

--- modules/system/building.nix
+++ modules/system/building.nix
@@ -4,6 +4,8 @@
   # !!! TARGETED FOR MY CURRENT CPU + RAM (Ryzen 9 5950x 64GB RAM) - IF I CHANGE PC CHANGE THIS !!!
   nix.settings = {
     max-jobs = 1;
-    cores = 16;
+    cores = 12;
   };
+
+  zramSwap.enable = true;
 }
