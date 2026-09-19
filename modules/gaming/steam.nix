2b4b00eb51f2c10399e24ee442c0ecbc91c8841b
MiguVT <contacto@miguvt.com>
2026-09-19 15:51:47 +0200

    fix(steam): Fixed System Resolution

--- modules/gaming/steam.nix
+++ modules/gaming/steam.nix
@@ -3,7 +3,7 @@
 {
   programs.steam = {
     enable = true;
-    package = inputs.millennium.packages.${pkgs.system}.millennium-steam.override {
+    package = inputs.millennium.packages.${pkgs.stdenv.hostPlatform.system}.millennium-steam.override {
       extraProfile = ''
         # Allows Monado/WiVRn to be used
         export PRESSURE_VESSEL_IMPORT_OPENXR_1_RUNTIMES=1
