e9e8c3be0c4424ceda56c00a046d388ea2f249f2
MiguVT <contacto@miguvt.com>
2026-09-20 04:20:31 +0200

    todo(forgejo): comment forgejo gitea actions for TODO

--- modules/system/forgejo.nix
+++ modules/system/forgejo.nix
@@ -9,25 +9,26 @@
     defaultNetwork.settings.dns_enabled = true;
   };
 
+  # TODO - setup Forgejo-runner
   # 2. Forgejo Actions Runner
-  services.gitea-actions-runner = {
-    package = pkgs.forgejo-runner;
+  #services.gitea-actions-runner = {
+  #  package = pkgs.forgejo-runner;
 
-    instances.default = {
-      enable = true;
-      name = "nixos-podman-runner";
+  #  instances.default = {
+  #    enable = true;
+  #    name = "nixos-podman-runner";
 
-      # Replace with your actual Forgejo server URL
-      url = "https://git.miguvt.com";
+  #    # Replace with your actual Forgejo server URL
+  #    url = "https://git.miguvt.com";
 
-      # Securely load the token from outside the world-readable Nix store
-      tokenFile = "/var/lib/forgejo-runner-token.env";
+  #    # Securely load the token from outside the world-readable Nix store
+  #    tokenFile = "/var/lib/forgejo-runner-token.env";
 
-      # Maps workflow 'runs-on' values to container images
-      labels = [
-        "ubuntu-latest:docker://ghcr.io/catthehacker/ubuntu:act-latest"
-        "nix-latest:docker://nixos/nix:latest"
-      ];
-    };
-  };
+  #    # Maps workflow 'runs-on' values to container images
+  #    labels = [
+  #      "ubuntu-latest:docker://ghcr.io/catthehacker/ubuntu:act-latest"
+  #      "nix-latest:docker://nixos/nix:latest"
+  #    ];
+  #  };
+  #};
 }
