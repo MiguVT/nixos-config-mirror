17f63757711fceb0c49a8c9303cb90d759bf2f41
MiguVT <contacto@miguvt.com>
2026-09-20 04:20:57 +0200

    storage(casensitive): Add ext4 image

--- modules/hardware/storage.nix
+++ modules/hardware/storage.nix
@@ -4,6 +4,21 @@
   fileSystems."/mnt/data" = {
     device = "/dev/disk/by-uuid/748e2f8b-c6e5-4af9-9242-d2d8065782c8";
     fsType = "btrfs";
-    options = [ "nofail" "compress=zstd" "noatime" ];
+    options = [
+      "nofail"
+      "compress=zstd"
+      "noatime"
+    ];
+  };
+
+  fileSystems."/mnt/casesensitive" = {
+    device = "/mnt/data/casesensitive.img";
+    fsType = "ext4";
+    depends = [ "/mnt/data" ];
+    options = [
+      "loop"
+      "nofail"
+      "noatime"
+    ];
   };
 }
